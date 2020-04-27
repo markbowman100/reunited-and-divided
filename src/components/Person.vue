38ed9bd4696a4a5821661dd6b2ae12cb6f11c27a
Mark Bowman <markbowman100@gmail.com>
2020-04-26 20:06:05 -0400

    Added person vue component

---
+++ src/components/Person.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="person" ref="richard">
+    <div class="logo-container">
+      <p class="md-display-1">
+        {{name}}
+      </p>
+      <img id="richard" alt="Richard" class="person-image" :src="imagePath">
+      <p class="md-subheading description" style="text-align:center;">
+        {{description}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Person extends Vue {
+  @Prop() private name!: string;
+  @Prop() private description!: string;
+  @Prop() private imagePath!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -15,59 +15,63 @@
     <div class="spacing-bottom border-bottom">
       <iframe width="100%" src="https://w.soundcloud.com/player/?url=https%3A//api-v2.soundcloud.com/tracks/797753905&amp;"></iframe>
     </div>
-    <div class="person" ref="richard">
-      <div class="logo-container">
-        <p class="md-display-1">
-          Richard Devoe
-        </p>
-        <img id="richard" alt="Richard" class="person-image" src="../assets/richard.jpg">
-        <p class="md-subheading description" style="text-align:center;">
-          Richard: Classified
-        </p>
-      </div>
-    </div>
-    <div class="person" ref="george">
-      <div class="logo-container">
-        <p class="md-display-1">
-          George Quick
-        </p>
-        <img id="george" alt="George" class="person-image" src="../assets/george.jpg">
-        <p class="md-subheading description" style="text-align:center;">
-          George Quick: I met Mark and Rich in the 8th grade, we made our own versions of RPGs and strategy games to play at lunch. <br>
-          In high school, we played Magic the Gathering, poker and Dungeons and Dragons. <br>
-          We were in choir and drama together, we hung out on weekends at my Dad's on the farm or at Rich's house. <br>
-          We spent the summers together on the farm or in summer school. Then the bad times came, we don't talk about the bad times. <br>
-          I got married, joined the Army and moved to Germany. I did IT in the Army, 25Q, worked in a signal company then an S6 shop. <br>
-          I spent 9 years and 2 deployments in the military, got remarried earned a bachelors and masters in Economics. <br>
-          I have 3 children a fantastic wife and work in data analytics in the IT audit department of a bank. <br>
-        </p>
-      </div>
-    </div>
-    <div class="person" ref="mark">
-      <div class="logo-container">
-        <p class="md-display-1">
-          Mark Bowman
-        </p>
-        <img id="mark" alt="Mark" class="person-image md-primary" src="../assets/mark.png">
-        <p class="md-subheading description" style="text-align:center;">
-          Mark Bowman: I don't remember exactly when I met Richard, but I didn't like him. <br>
-          I met George in 8th grade, and he invited me to play games. I found out later that Richard was invited too.  <br>
-          I was immediately skeptical of George at that point, but I gave it a shot anyway. It was a good thing.  <br>
-          Within a short period of time, we became good friends, and we continued playing games,  <br>
-          poker, dungeons and dragons, custom RPGs, video games, etc.  <br>
-          Then George destroyed everything. He got married. Then we all joined the military.  <br>
-          I served in the United States Air Force as Security Forces for six years.  <br>
-          I met my wife playing Star Wars The Old Republic. I went to college for Information Systems,  <br>
-          and I sought a career as a Software Engineer. I've been developing software ever since.  <br>
-          My wife and I got married in 2017, and my daughter was born in 2019. <br>
-        </p>
-      </div>
+    <div v-for="person in personInfo" :key="person">
+      <Person :description="person.description" :name="person.name" :imagePath="person.imagePath"></Person>
     </div>
   </div>
 </template>
 
+<script>
+  import Person from '../components/Person'
+
+  export default {
+    name: 'About',
+    data: function() {
+      const personData = [
+        {
+          description:"Classified",
+          name:"Richard Devoe",
+          imagePath:"img/richard.jpg"
+        },
+        {
+          description:`George Quick: I met Mark and Rich in the 8th grade, we made our own versions of RPGs and strategy games to play at lunch. <br>
+            In high school, we played Magic the Gathering, poker and Dungeons and Dragons. <br>
+            We were in choir and drama together, we hung out on weekends at my Dad's on the farm or at Rich's house. <br>
+            We spent the summers together on the farm or in summer school. Then the bad times came, we don't talk about the bad times. <br>
+            I got married, joined the Army and moved to Germany. I did IT in the Army, 25Q, worked in a signal company then an S6 shop. <br>
+            I spent 9 years and 2 deployments in the military, got remarried earned a bachelors and masters in Economics. <br>
+            I have 3 children a fantastic wife and work in data analytics in the IT audit department of a bank. <br>`,
+          name:"George Quick",
+          imagePath:"img/george.jpg"
+        },
+        {
+          description:`Mark Bowman: I don't remember exactly when I met Richard, but I didn't like him. <br>
+            I met George in 8th grade, and he invited me to play games. I found out later that Richard was invited too.  <br>
+            I was immediately skeptical of George at that point, but I gave it a shot anyway. It was a good thing.  <br>
+            Within a short period of time, we became good friends, and we continued playing games,  <br>
+            poker, dungeons and dragons, custom RPGs, video games, etc.  <br>
+            Then George destroyed everything. He got married. Then we all joined the military.  <br>
+            I served in the United States Air Force as Security Forces for six years.  <br>
+            I met my wife playing Star Wars The Old Republic. I went to college for Information Systems,  <br>
+            and I sought a career as a Software Engineer. I've been developing software ever since.  <br>
+            My wife and I got married in 2017, and my daughter was born in 2019. <br>`,
+          name:"Mark Bowman",
+          imagePath:"img/mark.png"
+        }
+      ];
+
+      return {
+        personInfo: personData
+      };
+    },
+    components: {
+      Person
+    }
+  }
+</script>
+
 <style scoped>
-.person-image {
+  .person-image {
     display: block;
     margin-left: auto;
     margin-right: auto;
